7337bf58ad50cd585a22eddaed18ec2415e5dee1
erlend <erlenrom@stud.ntnu.no>
2024-04-08 15:41:12 +0200

    Added server implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,7 @@
 package main
+
+import "webhooks/cmd/api/server"
+
+func main() {
+	server.StartServer()
+}
--- cmd/api/server/server.go
+++ cmd/api/server/server.go
@@ -1,1 +1,17 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"webhooks/internal/config"
+	"webhooks/internal/http/routes"
+)
+
+func StartServer() {
+	config := config.NewConfig()
+	router := routes.NewRouter(*config)
+
+	log.Printf("Server started on port %s...\n", router.Port)
+	http.ListenAndServe(fmt.Sprintf(":%s", router.Port), router)
+}
