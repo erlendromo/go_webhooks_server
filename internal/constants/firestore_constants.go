29ad261bbb2597f216a1fd560f9e3bd3ce43eadf
erlend <erlenrom@stud.ntnu.no>
2024-04-10 11:54:50 +0200

    Renamed to comply with go naming conventions

--- internal/constants/firestore_constants.go
+++ internal/constants/firestore_constants.go
@@ -1,9 +1,15 @@
 package constants
 
+// Collections
 const WEBHOOK_TRIGGERS_COLLECTION = "webhook_triggers"
 
-const TIMESTAMP = "Timestamp"
+// Utils
+const (
+	TIMESTAMP               = "Timestamp"
+	FIRESTORE_REQUEST_LIMIT = 20
+)
 
+// Errors
 const (
 	FAILED_FETCH_DOCS = "failed to fetch documents"
 	FAILED_DECODE_DOC = "Failed to decode document"
