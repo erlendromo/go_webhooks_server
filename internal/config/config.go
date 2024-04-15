4f42878a1290063185d83dd8ef7386bfdd7698ca
erlend <erlenrom@stud.ntnu.no>
2024-04-15 15:28:01 +0200

    fixed config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,18 +11,18 @@ type Config struct {
 }
 
 func NewConfig() (*Config, error) {
-	p := os.Getenv("PORT")
-	if p == "" {
-		return nil, fmt.Errorf("unable to set server-port")
+	port, present := os.LookupEnv("PORT")
+	if !present {
+		port = "8081"
 	}
 
-	dbToken := os.Getenv("FIRESTORE_ACCESS_TOKEN")
-	if dbToken == "" {
+	dbToken, present := os.LookupEnv("FIRESTORE_ACCESS_TOKEN")
+	if !present {
 		return nil, fmt.Errorf("unable to load firestore access token")
 	}
 
 	return &Config{
-		Port:          p,
+		Port:          port,
 		DatabaseToken: dbToken,
 	}, nil
 }
